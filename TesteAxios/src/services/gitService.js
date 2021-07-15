43665c44dbbbbab32014c5209384588ac55c768f
Diego <diego.canedo86@gmail.com>
2021-07-14 21:02:39 -0300

    exemplo axios crud

--- TesteAxios/src/services/gitService.js
+++ TesteAxios/src/services/gitService.js
@@ -4,6 +4,22 @@ const GIT_URL = "https://api.github.com/orgs/rocketseat-education/repos";
 
 //https://api.github.com/users/DiegoCanedo/repos
 
+// const listarID = (id) => {
+//     return http.get(`${ULR_PRODUTO}/${id}`);
+//   };
+  
+//   const incluir = (data) => {
+//     return http.post(ULR_PRODUTO, data);
+//   };
+  
+//   const atualizar = (id, data) => {
+//     return http.put(`${ULR_PRODUTO}/${id}`, data);
+//   };
+  
+//   const deletar = (id) => {
+//     return http.delete(`${ULR_PRODUTO}/${id}`);
+//   };
+
 const listarRepositorios = () => {
     return http.get(GIT_URL);
 };
